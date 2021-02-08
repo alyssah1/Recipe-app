d18199cdc6321e038fcd447aee87e8d4e4c99a92
Alyssa Hollowell <alyssa.hollowell2@gmail.com>
2021-02-08 14:41:25 -0600

    added starter code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const API_ID = "437209d7";
+  const API_KEY= "17286e1704e9a158e6171ec68fc03c7d";
+
+
+  const exampleRequest = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello</h1>
     </div>
   );
 }
