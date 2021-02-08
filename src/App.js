42cf9302cf950f0b29e1ec66edb8d543b8fff296
Alyssa Hollowell <alyssa.hollowell2@gmail.com>
2021-02-08 14:57:43 -0600

    added jumbotron and search button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-
+import React from "react";
 import './App.css';
+import Jumbotron from './components/jumbotron';
 
 function App() {
 
-  const API_ID = "437209d7";
-  const API_KEY= "17286e1704e9a158e6171ec68fc03c7d";
+  const APP_ID = "437209d7";
+  const APP_KEY= "17286e1704e9a158e6171ec68fc03c7d";
 
 
-  const exampleRequest = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
 
   return (
     <div className="App">
-    <h1>Hello</h1>
+    <Jumbotron/>
+    <form className="search-form">
+      <input type="text" className="search-bar"></input>
+      <button type="submit" className="btn btn-danger m-4">Search</button>
+    </form>
+
     </div>
   );
 }
---
+++ src/components/jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Recipe App</h1>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Jumbotron;
